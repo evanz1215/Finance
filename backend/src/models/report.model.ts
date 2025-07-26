1df3cbb65db7de61735b00c9d819742fb5a64fea
Evan <evanz1215@gmail.com>
2025-07-26 15:38:32 +0800

    新增使用者、報告、報告設定及交易模型，並實作密碼加密與貨幣格式轉換功能

---
+++ backend/src/models/report.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+export enum ReportStatusEnum {
+  SENT = "SENT",
+  PENDING = "PENDING",
+  FAILED = "FAILED",
+}
+
+export interface ReportDocument extends mongoose.Document {
+  userId: mongoose.Types.ObjectId;
+  period: string;
+  sentDate: Date;
+  status: keyof typeof ReportStatusEnum;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const reportSchema = new Schema<ReportDocument>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    period: {
+      type: String,
+      required: true,
+    },
+    sentDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(ReportStatusEnum),
+      default: ReportStatusEnum.PENDING,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ReportModel = mongoose.model<ReportDocument>("Report", reportSchema);
+export default ReportModel;
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,3 +1,62 @@
-import { Document } from "mongoose";
+import mongoose, { Document } from "mongoose";
+import { compareValue, hashValue } from "../utils/bcrypt";
 
-export interface UserDocument extends Document {}
+export interface UserDocument extends Document {
+  name: string;
+  email: string;
+  password: string;
+  profilePicture: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword: (password: string) => Promise<boolean>;
+  omitPassword: () => Omit<UserDocument, "password">;
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      select: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    if (this.password) {
+      this.password = await hashValue(this.password);
+    }
+  }
+  next();
+});
+
+userSchema.methods.omitPassword = function (): Omit<UserDocument, "password"> {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject as Omit<UserDocument, "password">;
+};
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  // 密碼比較邏輯
+  return compareValue(password, this.password);
+};
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
+export default UserModel;
